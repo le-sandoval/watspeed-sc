180a7f2f2eff1c2000a34b29d7e838c53977a343
Leonardo <e.sandoval.leonardo@gmail.com>
2025-01-07 13:41:13 -0500

    Improve file size calculation logic for security
    Refactor: Improve file size calculation logic for security and simplicity
    - replaced system with fopen, fseek, ftell
    - error handling for invalid inputs

--- simlab2e1.c
+++ simlab2e1.c
@@ -11,7 +11,26 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+    //char cmd[BUFSIZE] = "wc -c < ";
+    //strcat(cmd, argv[1]);
+    //system(cmd);
+
+    //Open file
+    FILE* file = fopen(argv[1], "rb");
+    if (!file) {
+        fprintf(stderr, "Error: Could not open the file. Please make sure file exists.\n");
+        return -1;
+    }
+    //Determine file size
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fclose(file);
+    //Print the size of the file
+    if (file_size < 0) {
+        fprintf(stderr, "Error: Could not figure out the file size.\n");
+        return -1;
+    }
+    printf("File size: %ld bytes", file_size);
+    return 0;
+
 }
